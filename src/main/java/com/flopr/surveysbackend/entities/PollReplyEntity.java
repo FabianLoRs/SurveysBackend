f520db258bc92cf32ebdd561ee85107a03e8b40d
Fabián López <122753033+FabianLoRs@users.noreply.github.com>
2025-03-27 14:06:21 -0300

    Endpoints for responding to surveys finished.

--- src/main/java/com/flopr/surveysbackend/entities/PollReplyEntity.java
+++ src/main/java/com/flopr/surveysbackend/entities/PollReplyEntity.java
@@ -18,8 +18,6 @@ import javax.persistence.OneToMany;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.Data;
 
 @EntityListeners(AuditingEntityListener.class)
